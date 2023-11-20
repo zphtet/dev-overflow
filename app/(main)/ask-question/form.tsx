872144f21780f613bcfe0471aeab2a07078beca5
zphtet <zinphtet.215108@gmail.com>
2023-11-20 22:34:59 +0630

    create tag input functionality

--- app/(main)/ask-question/form.tsx
+++ app/(main)/ask-question/form.tsx
@@ -14,10 +14,13 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { KeyboardEvent, KeyboardEventHandler, useRef } from "react";
+import { Badge } from "@/components/ui/badge";
+import { ShieldCloseIcon, X } from "lucide-react";
 const formSchema = z.object({
   title: z.string().min(5, { message: "must be 5 chars or more" }),
   explaination: z.string().min(100, { message: "must be 100 chars or more" }),
-  tags: z.string().min(5, { message: "must be 5 chars or more" }),
+  tags: z.array(z.string().min(1).min(15)).min(1).max(3),
 });
 
 const AskQuestionFrom = () => {
@@ -26,8 +29,12 @@ const AskQuestionFrom = () => {
     resolver: zodResolver(formSchema),
     defaultValues: {
       title: "",
+      explaination: "",
+      tags: [],
     },
   });
+
+  const editorRef = useRef(null);
 
   // 2. Define a submit handler.
   function onSubmit(values: z.infer<typeof formSchema>) {
@@ -35,6 +42,30 @@ const AskQuestionFrom = () => {
     // ✅ This will be type-safe and validated.
     console.log(values);
   }
+
+  const onChnageHandler = (e: KeyboardEvent<HTMLInputElement>, field: any) => {
+    if (e.key === "Enter" && field.name === "tags") {
+      e.preventDefault();
+      // alert("you are pressing on Tags Input");
+      const tagInput = e.target as HTMLInputElement;
+      const newTag = tagInput.value.trim();
+      if (newTag === "") return;
+
+      if (newTag.length > 15) {
+        return form.setError("tags", {
+          message: "tag must be at most 15 characters long",
+          type: "required",
+        });
+      }
+      if (!field.value.includes(newTag)) {
+        form.setValue("tags", [...field.value, newTag]);
+        tagInput.value = "";
+        form.clearErrors("tags");
+      } else {
+        form.trigger();
+      }
+    }
+  };
 
   return (
     <Form {...form}>
@@ -74,9 +105,11 @@ const AskQuestionFrom = () => {
               <FormControl>
                 <Editor
                   apiKey="nrhs7e2w9iyjoxp4hvqzxewfcgk76gy8299j834brpi0fptr"
-                  //   onInit={(evt, editor) => (editorRef.current = editor)}
-                  //   initialValue="<p>write the detail explaination ...</p>"
-
+                  onInit={(evt, editor) => {
+                    // @ts-ignore
+                    return (editorRef.current = editor);
+                  }}
+                  initialValue=""
                   init={{
                     height: 350,
                     menubar: false,
@@ -107,10 +140,10 @@ const AskQuestionFrom = () => {
                       "codesample bold italic forecolor | alignleft aligncenter " +
                       "alignright alignjustify | bullist numlist outdent indent | " +
                       "removeformat | preview | help",
-                    content_style:
-                      "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
+                    // content_style:
+                    //   "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
                   }}
-                  {...field}
+                  // {...field}
                 />
               </FormControl>
               <span className="text-[12px] text-light-500">
@@ -131,16 +164,45 @@ const AskQuestionFrom = () => {
                 Tags <span className="text-red-600">*</span>
               </FormLabel>
               <FormControl>
-                <Input
-                  placeholder="tags "
-                  {...field}
-                  className="focus:ring-none focus-visible:outline-none focus-visible:ring-0 outline-none py-6 focus-visible:ring-transparent"
-                />
+                <>
+                  <Input
+                    placeholder="Add tags... "
+                    // {...field}
+                    // {field.value.in}
+                    onKeyDown={(e) => onChnageHandler(e, field)}
+                    className="focus:ring-none focus-visible:outline-none focus-visible:ring-0 outline-none py-6 focus-visible:ring-transparent"
+                  />
+                  {field.value.length > 0 && (
+                    <div className="flex items-center gap-3">
+                      {field?.value?.map((tag) => {
+                        return (
+                          <div
+                            key={tag}
+                            className="px-4 py-1 flex items-center gap-3 rounded bg-light-700 text-light-400 dark:bg-dark-400 dark:text-white"
+                          >
+                            {tag}
+                            <button
+                              onClick={() => {
+                                const filteredValues = field.value.filter(
+                                  (tagItem) => tagItem !== tag
+                                );
+                                form.setValue("tags", [...filteredValues]);
+                              }}
+                            >
+                              <X width={"16px"} height={"16px"} />
+                            </button>
+                          </div>
+                        );
+                      })}
+                    </div>
+                  )}
+                </>
               </FormControl>
               <span className="text-[12px] text-light-500">
                 Add up to 3 tags to describe what your question is about. You
                 need to press enter to add a tag.
               </span>
+
               <FormMessage />
             </FormItem>
           )}
